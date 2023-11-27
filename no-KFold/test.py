d70d1a593fafc7bba1ff2355d7f7aadb32f71f57
DIB <74963727+DIBreeding@users.noreply.github.com>
2023-11-27 11:22:08 +0800

    Add files via upload

---
+++ no-KFold/test.py
@@ -0,0 +1,35 @@
+
+import pandas as pd
+import numpy as np
+import importlib.util
+pyc_path = 'ISSA-XGBOOST-no-KFold.pyc'
+spec = importlib.util.spec_from_file_location('module_name', pyc_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+
+
+#The directory where the genotype data is located
+GenePath=r"testdata\06potato2\genotypes.csv"
+#The directory where the performance data is located
+PhenotypePath=r"testdata\06potato2\TW.csv"
+#Result output directory
+outpath=r"06potato2\\"
+
+
+#Only these few parameters need to be set, and the rest of the parameters adopt the default parameters
+pop_size=50
+max_iters=100
+test_size=0.1 #The ratio of the training set to the test set, 0.1 represents 9:1
+
+lb=[0.1,2,0.01,0.1,0.5]
+ub=[1,30,1,1,1]
+
+X=pd.read_csv(GenePath).iloc[:,1:]  #read csv文件
+y=pd.read_csv(PhenotypePath,engine='python').iloc[:,0]
+
+
+module.MYISSA(p_X=X,p_y=y,p_test_size=test_size,p_lb=lb,p_ub=ub,p_step=int(X.shape[1]/125)+2,p_pop_size=pop_size, p_max_iters=max_iters,output_path=outpath,p_early_stop_threshold1=50)
+
+
+
